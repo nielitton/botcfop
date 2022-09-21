2058785b712224b70c1da3ce6be5625c04c2829f
Nielitton Maxco <nielitonmaxco@gmail.com>
2022-09-21 15:13:06 -0300

    verifyng SP CFOP

--- botcfop.py
+++ botcfop.py
@@ -39,24 +39,31 @@ with sync_playwright() as p:
     pageBling.fill("input[id='senha']", "Psfm1234@")
     pageBling.wait_for_timeout(2000)
     pageBling.click("button[name='enviar']")
-    pageBling.wait_for_timeout(5000)
+    pageBling.wait_for_timeout(2000)
     pageBling.locator("//html/body/div[4]/div[1]/div/i").click()
     pageBling.wait_for_timeout(2000)
-    while (fim <= 3):
+    while (fim <= 100):
         while (tableRow <= 100):
             pageBling.locator(f"xpath=/html/body/div[5]/div[4]/div[2]/div[2]/table/tbody/tr[{tableRow}]/td[2]").click()
             pageBling.wait_for_timeout(2000)
             ufEmit = pageBling.input_value("select[id=cte_emit_UF]")
             ufRem = pageBling.input_value("select[id=cte_rem_UF]")
             cfop = pageBling.input_value("input[id='cte_CFOP']")
-            if (ufEmit == "ES" and ufRem == "ES" and cfop != "1353"):
-                pageBling.fill("input[id='cte_CFOP']", "1353")
-                pageBling.click("button[id='botaoCancelar']")
+            if (ufEmit == "ES" and ufRem == "ES" and cfop != "1354"):
+                pageBling.fill("input[id='cte_CFOP']", "1354")
+                pageBling.wait_for_timeout(2000)
+                pageBling.click("button[id='botaoSalvar']")
+                tableRow += 1
+            elif (ufEmit != "ES" and ufRem != "ES" and cfop != "2354"):
+                pageBling.fill("input[id='cte_CFOP']", "2354")
+                pageBling.wait_for_timeout(2000)
+                pageBling.click("button[id='botaoSalvar']")
+                pageBling.wait_for_timeout(2000)
                 tableRow += 1
             elif (cfop == "1353"):
                 pageBling.close()
         pageBling.wait_for_timeout(2000)
         pageBling.locator("//html/body/div[5]/div[4]/div[2]/div[2]/nav/ul/li[4]/span").click()
-        tableRow = 98
+        tableRow = 1
         pageBling.wait_for_timeout(2000)
         fim += 1
