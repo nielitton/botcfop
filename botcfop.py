cd4323a089d1e7f84580bbc64d865191a9387a9c
Nielitton Maxco <nielitonmaxco@gmail.com>
2022-09-22 16:19:21 -0300

    Final when a variable reaches 5

--- botcfop.py
+++ botcfop.py
@@ -2,6 +2,7 @@ from playwright.sync_api import sync_playwright
 
 tableRow = 1
 fim = 1
+final = 0
 
 
 # Para pegar os arquivos do arquivei no futuro
@@ -33,13 +34,13 @@ with sync_playwright() as p:
     pageBling = browser.new_page()
     pageBling.goto('https://www.bling.com.br/ctes.php#list')
     pageBling.wait_for_timeout(2000)
-    pageBling.fill("input[id='username']", "******")
+    pageBling.fill("input[id='username']", "******@*****atacado.com.br")
     pageBling.wait_for_timeout(2000)
-    pageBling.fill("input[id='senha']", "******")
+    pageBling.fill("input[id='senha']", "**************")
     pageBling.wait_for_timeout(2000)
     pageBling.click("button[name='enviar']")
     pageBling.wait_for_timeout(2000)
-    while (fim <= 100):
+    while (final <= 5):
         while (tableRow <= 100):
             pageBling.locator(f"xpath=/html/body/div[5]/div[4]/div[2]/div[2]/table/tbody/tr[{tableRow}]/td[2]").click()
             pageBling.wait_for_timeout(2000)
@@ -57,8 +58,10 @@ with sync_playwright() as p:
                 pageBling.click("button[id='botaoSalvar']")
                 pageBling.wait_for_timeout(2000)
                 tableRow += 1
+            elif (cfop == "2353" or cfop == "1553"):
+                final += 1
+                tableRow += 1
         pageBling.wait_for_timeout(2000)
         pageBling.locator("//html/body/div[5]/div[4]/div[2]/div[2]/nav/ul/li[4]/span").click()
         tableRow = 1
         pageBling.wait_for_timeout(2000)
-        fim += 1
